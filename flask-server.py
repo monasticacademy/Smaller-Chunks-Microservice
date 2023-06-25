eebc75470d406e4e0fa58ae06d97c7260dacf2be
Itshalffull <magicmattg@gmail.com>
2023-06-25 19:35:33 -0400

    Update flask-server.py
    Use new chatgpt code

--- flask-server.py
+++ flask-server.py
@@ -1,79 +1,59 @@
-from flask import Flask, request, jsonify
-from flask_httpauth import HTTPBasicAuth
-from werkzeug.security import generate_password_hash, check_password_hash
-import re
-import json
-from datetime import datetime, timedelta
-import requests
+from flask import Flask, request, jsonify, make_response
+from functools import wraps
 
 app = Flask(__name__)
-auth = HTTPBasicAuth()
 
-users = {
-    "username": generate_password_hash("password")
-}
+# Basic HTTP Authentication
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
-@auth.verify_password
-def verify_password(username, password):
-    if username in users and check_password_hash(users.get(username), password):
-        return username
+def check_auth(username, password):
+    # Replace 'admin' and 'secret' with your desired username and password
+    return username == 'admin' and password == 'secret'
 
-def parse_srt(srt_text):
-    chunks = re.split(r'(\d\d:\d\d:\d\d,\d\d\d --> \d\d:\d\d:\d\d,\d\d\d)', srt_text)[1:]
-    subtitles = []
-    for i in range(0, len(chunks), 2):
-        times, text = chunks[i], chunks[i+1].strip()
-        sentences = re.split(r'(?<=\D) \d+ ', text)
-        for sentence in sentences:
-            sentence = re.sub(r' \d+$', '', sentence)
-            if sentence:
-                subtitles.append((times, sentence))
-    return subtitles
+def authenticate():
+    message = {'message': "Authentication required."}
+    resp = make_response(jsonify(message))
+    resp.status_code = 401
+    resp.headers['WWW-Authenticate'] = 'Basic realm="Example"'
+    return resp
 
-def calculate_pauses(subtitles):
-    format_str = "%H:%M:%S,%f"
-    guidance_chunks = []
-    pauses = []
-    previous_end = None
-    for times, text in subtitles:
-        start, end = re.findall(r'(\d\d:\d\d:\d\d,\d\d\d)', times)
-        start = datetime.strptime(start, format_str)
-        end = datetime.strptime(end, format_str)
-        if previous_end is not None:
-            pause = round((start - previous_end).total_seconds())
-            if pause < 3 and guidance_chunks:
-                guidance_chunks[-1] += " " + text
-            else:
-                pauses.append(pause)
-                guidance_chunks.append(text)
+# Modify guidance function
+def modify_guidance(json_input):
+    guidance_chunks = json_input.get("guidance_chunks", [])
+    pauses = json_input.get("pauses", [])
+    new_guidance_chunks = []
+    new_pauses = []
+    for chunk, pause in zip(guidance_chunks, pauses):
+        if len(chunk) > 250:
+            sentences = chunk.split(". ")
+            for i, sentence in enumerate(sentences):
+                new_guidance_chunks.append(sentence)
+                if i == len(sentences) - 1:
+                    new_pauses.append(pause)
+                else:
+                    new_pauses.append(0)
         else:
-            guidance_chunks.append(text)
-        previous_end = end
-    return guidance_chunks, pauses
+            new_guidance_chunks.append(chunk)
+            new_pauses.append(pause)
+    json_input["guidance_chunks"] = new_guidance_chunks
+    json_input["pauses"] = new_pauses
+    return json_input
 
-def download_srt_file(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    else:
-        return None
+# API endpoint
+@app.route('/modify_guidance', methods=['POST'])
+@requires_auth
+def api_modify_guidance():
+    json_input = request.json
+    result = modify_guidance(json_input)
+    return jsonify(result)
 
-@app.route('/parse_srt', methods=['POST'])
-@auth.login_required
-def process_srt_file():
-    data = request.get_json()
-    file_url = data.get('file_url')
-
-    if not file_url:
-        return jsonify({"error": "No file URL provided"}), 400
-
-    srt_text = download_srt_file(file_url)
-    if not srt_text:
-        return jsonify({"error": "Failed to download the SRT file"}), 500
-
-    subtitles = parse_srt(srt_text)
-    guidance_chunks, pauses = calculate_pauses(subtitles)
-    return jsonify({'guidance_chunks': guidance_chunks, 'pauses': pauses}), 200
-
+# Run the Flask app
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.getenv("PORT")))
